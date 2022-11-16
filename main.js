3e769d1be076e3efb707d75ab5edbe4fe12533db
devnomi1 <noumanijaz.dev@outlook.com>
2022-11-16 15:55:01 +0500

    creating new sign in form

---
+++ main.js
@@ -0,0 +1,20 @@
+// let items = [
+//   { color: 'red', type: 'tv', age: 18 },
+//   { color: 'sliver', type: 'mobile', age: 12 },
+//   { color: 'green', type: 'laptop', age: 14 },
+// ];
+
+// let newItems = items;
+// console.log(newItems);
+
+// var x = 2;
+// var y = '2';
+// console.log(x == y);
+// console.log(x === y);
+// console.log(x + y);
+// function slove(x, y) {
+//   return console.log(x + y);
+// }
+
+// slove();
+// console.log(x&&y);
